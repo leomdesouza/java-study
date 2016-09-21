01467de6953317ed2b04ac3c7a19971e46dc4154
leomdesouza <leosouza@kiddolabs.com>
2016-09-20 21:25:10 -0300

    first-java-program

---
+++ first-java-program/Hello/src/com/myapp/javaApp/HelloAndroid.java
@@ -0,0 +1,13 @@
+package com.myapp.javaApp;
+
+// this is my first java program
+
+public class HelloAndroid {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello, Java! You are great!");
+
+	}
+
+}
